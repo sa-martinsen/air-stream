7c4edc1f022da1a5b28b8edd842153d655b9b5db
Полицинский Артем Сергеевич <as_policinskij@bingo-boom.ru>
2018-08-30 20:39:09 +0300

    - webpack conf - fixed

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,3 @@
-import "./set_immediate.mjs"
 export {default as Observable, combine, merge} from "./observable/index.mjs"
 export {default as stream} from "./stream/index.mjs"
 export {default as Stream, keyF, keyA} from "./observable/index.mjs"
