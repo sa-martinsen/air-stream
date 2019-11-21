3d1f30ee802601c667b2f6cad4c60096d9534a19
sa_martinsen <sa_martinsen@bingo-boom.com>
2019-11-21 18:20:52 +0300

    - emitIfChanged

--- src/utils.mjs
+++ src/utils.mjs
@@ -36,6 +36,26 @@ const deepEqual = function (x, y) {
     return false;
 };
 
+export function equal(a, b) {
+  if(a === b) {
+    return true;
+  }
+  else {
+    if(Array.isArray(a)) {
+      return a.length === b.length && a.every( (a, i) => equal( a, b[i] ) );
+    }
+    else if(
+      typeof a === "object" && a !== null && b !== null && a.constructor === Object
+    ) {
+      const keysA = Object.keys(a);
+      const keysB = Object.keys(b);
+      return keysA.length === keysB.length &&
+        keysA.every( k => equal(a[k], b[k]) )
+    }
+    return false;
+  }
+}
+
 export const streamEqual = (done, source, data = [], options = {}) => {
   const defaultOptions = {
     delta: 100, // ms
